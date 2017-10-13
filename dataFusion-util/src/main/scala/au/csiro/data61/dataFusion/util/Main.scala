955fa048dc7a1c28fb263386af2707434f4c66ac
bac003 <neil.bacon@data61.csiro.au>
2017-10-13 13:26:13 +1100

    parallelize

--- dataFusion-util/src/main/scala/au/csiro/data61/dataFusion/util/Main.scala
+++ dataFusion-util/src/main/scala/au/csiro/data61/dataFusion/util/Main.scala
@@ -20,15 +20,15 @@ import java.io.InputStream
 object Main {
   private val log = Logger(getClass)
   
-  case class CliOption(hits: Option[File], output: Option[File])
+  case class CliOption(hits: Option[File], output: Option[File], numWorkers: Int)
   
-  val defaultCliOption = CliOption(None, None)
-  
+  val defaultCliOption = CliOption(None, None, Runtime.getRuntime.availableProcessors)
+  val defGazOut = "gaz.json"
   val parser = new scopt.OptionParser[CliOption]("util") {
     head("util", "0.x")
     opt[File]("hits") action { (v, c) =>
-      c.copy(hits = Some(v), output = c.output.orElse(Some(new File("gaz.json"))))
-    } text (s"read tika/ner json on stdin and write same augmented with NER data derived from hits (arg is input hits.json file)")
+      c.copy(hits = Some(v), output = c.output.orElse(Some(new File(defGazOut))))
+    } text (s"read hits from specified file, read tika/ner json from stdin and write it augmented with NER data derived from hits to output file (defaults to $defGazOut)")
     opt[File]("output") action { (v, c) =>
       c.copy(output = Some(v))
     } text (s"output JSON file")
@@ -37,25 +37,40 @@ object Main {
     
   def main(args: Array[String]): Unit = {
     try {
-      for {
-        c <- parser.parse(args, defaultCliOption)
-        hFile <- c.hits
-        hIn <- managed(new FileInputStream(hFile))
-        oFile <- c.output
-        w <- managed(bufWriter(oFile))
-        augment = augmentWithHits(hitsMap(hitIter(hIn))) _
-        d <- docIter(System.in)
-      } {
-        w.write(augment(d).toJson.compactPrint)
-        w.write('\n')
+      log.info("start")
+      parser.parse(args, defaultCliOption).foreach { c => 
+        log.info(s"main: cliOptions = $c")
+        if (c.hits.isDefined) doHits(c)
+        else log.info("Nothing to do. Try --help")
+      log.info("complete")
       }
     } catch {
       case NonFatal(e) => log.error("Main.main:", e)
     }
   }
   
+  def doHits(c: CliOption) = {
+    for {
+      hFile <- c.hits
+      hIn <- managed(new FileInputStream(hFile))
+      oFile <- c.output
+      w <- managed(bufWriter(oFile))
+    } {
+      val hMap = hitsMap(hitIter(hIn))
+      log.info(s"doHits: loaded hits for ${hMap.size} docs/embedded docs")
+      val augment: Doc => Doc = augmentWithHits(hMap)
+
+      val in = Source.fromInputStream(System.in, "UTF-8").getLines
+      val work: String => String = s => augment(s.parseJson.convertTo[Doc]).toJson.compactPrint
+      val out: String => Unit = json => {
+        w.write(json)
+        w.write('\n')
+      }
+      doParallel(in, work, out, "done", "done", c.numWorkers)
+    }
+  }
+  
   def hitIter(hIn: InputStream): Iterator[PHits] = Source.fromInputStream(hIn, "UTF-8").getLines.map(_.parseJson.convertTo[PHits])
-  def docIter(dIn: InputStream): Iterator[Doc] = Source.fromInputStream(dIn, "UTF-8").getLines.map(_.parseJson.convertTo[Doc])
   
   /** idEmbIdx -> extRefId, score, typ, lposdoc */
   type HitsMap = Map[IdEmbIdx, Seq[(Long, Float, String, LPosDoc)]]
